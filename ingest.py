4090101885331fcc721e249f9c6f8e1394d1b041
Hugo BACARD <hbacard@pm.me>
2024-05-30 14:27:16 +0200

    change green to yellow taxi

--- ingest.py
+++ ingest.py
@@ -4,7 +4,8 @@ from typing import List
 
 import pandas as pd
 
-base_url_green_taxi = 'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata'
+# base_url_green_taxi = 'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata'
+base_url_yellow_taxi = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata'
 
 if 'data_loader' not in globals():
     from mage_ai.data_preparation.decorators import data_loader
@@ -16,7 +17,7 @@ def ingest_files(**kwargs) -> pd.DataFrame:
     for year, months in [(2024, (3, 4))]:
         for i in range(*months):
             response = requests.get(
-             base_url_green_taxi + f'_{year}-{i:02d}.parquet'
+                base_url_yellow_taxi + f'_{year}-{i:02d}.parquet'
             )
 
             if response.status_code != 200:
